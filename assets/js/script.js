bbe49bb3b1d53cdad573cd4e4e7c1b727fb47c9b
Linh Tran <linhtran@Linhs-Air.home>
2023-02-09 14:30:57 -0800

    Adding fucntion for grabbing astronomy random photo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,35 @@
+var APIKey = "S7totvp99lZVSFYYOyQf6MWFjfqmkXftXQguSU6k";
+var searchButtonEl = $('#search-button');
 var dateInputEl = $('#datepicker');
 
 // Date autocomplete
 $(function () {
     dateInputEl.datepicker({
-      changeMonth: true,
-      changeYear: true,
+        changeMonth: true,
+        changeYear: true,
+        dateFormat: 'yy-mm-dd'
     });
-  });
+});
+
+searchButtonEl.on("click", function (event) {
+    event.preventDefault();
+
+    var searchValue = dateInputEl.val();
+    console.log(searchValue);
+
+    getAstronomyPhoto();
+});
+
+// Pulls image from selected date
+function getAstronomyPhoto() {
+    console.log(dateInputEl)
+    var queryURL = "https://api.nasa.gov/planetary/apod?date=" + dateInputEl.val() + "&api_key=" + APIKey;
+
+    fetch(queryURL)
+        .then(res => res.json())
+        .then(response => {
+            document.querySelector('#space-photo').src = response.hdurl
+            // Facts about the photo
+            document.querySelector('#photo-description').textContent = response.explanation
+        })
+}

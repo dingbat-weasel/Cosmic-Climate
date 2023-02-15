2e693185f767198f0f45cd8bb1c68d684ff994eb
Isaac Perk <97003977+isaacperk@users.noreply.github.com>
2023-02-14 21:11:19 -0800

    further cssing

--- assets/js/script.js
+++ assets/js/script.js
@@ -229,7 +229,7 @@ function writeMartianWeather() {
   ];
 
   function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max + 1);
   }
 
   let weatherToday = marsWeather[getRandomInt(marsWeather.length)];
